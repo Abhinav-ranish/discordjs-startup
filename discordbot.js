dcf28f8c656073f67549a40f6ef4d186a57e7a49
Abhinav-ranish <65065563+Abhinav-ranish@users.noreply.github.com>
2020-12-13 20:02:09 +0300

    Update discordbot.js

--- discordbot.js
+++ discordbot.js
@@ -4,7 +4,11 @@ const token = process.env.DISCORD_BOT_TOKEN;
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+client.user.setActivity("BLAZE ROCKS prefix ?!", {
+  type: "STREAMING",
+  url: "https://www.twitch.com/monstercat"
 });
+
 
 client.on('message', (msg) => {
   if (msg.content === '/ping') {
